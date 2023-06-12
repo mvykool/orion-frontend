e0ba7dd23179d30608544e4c9d9bbf5117e56729
maicol hernandez <maykolandres4@gmail.com>
2023-06-12 17:44:34 -0500

    set routes up

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { createBrowserRouter,RouterProvider, } from "react-router-dom";
 import './index.css'
+
+//routes
+import Login from './routes/Login';
+import Home from './routes/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
