0b639638d388dfbbe6b2c6752fbe32ff099f4819
maicol hernandez <maykolandres4@gmail.com>
2023-06-19 07:52:49 -0500

    changed to history back

--- src/components/BackBtn.tsx
+++ src/components/BackBtn.tsx
@@ -1,11 +1,15 @@
-import { Link } from "react-router-dom"
 
 
 const BackBtn = () => {
+
+	const handleBackBtn = () => {
+		history.back();
+	}
+
 return (
-	<Link to={"/"} className="h-14 w-14 bg-white rounded-full flex justify-center items-center fixed left-8 top-10">
+	<button onClick={() => handleBackBtn()} className="h-14 w-14 bg-white rounded-full flex justify-center items-center fixed left-8 top-10">
 		Back
-	</Link>
+	</button>
 )
 }
 
